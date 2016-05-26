40e99e38390420a0cd7ceb7b2b225d8160c95218
Rémy HUBSCHER <rhubscher@mozilla.com>
2016-05-26 16:22:43 +0200

    Back to development: 1.3.0

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -3,6 +3,12 @@ CHANGELOG
 
 This document describes changes between each past release.
 
+1.3.0 (unreleased)
+==================
+
+- Nothing changed yet.
+
+
 1.2.0 (2016-05-26)
 ==================
 
--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ ENTRY_POINTS = {
 
 
 setup(name='amo2kinto',
-      version='1.2.0',
+      version='1.3.0.dev0',
       description='Generate a blocklists.xml file from the Kinto collections.',
       long_description=README + "\n\n" + CHANGELOG,
       license='Apache License (2.0)',
