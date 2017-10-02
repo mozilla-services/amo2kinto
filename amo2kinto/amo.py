9465f43729dc655ff24d280ceafaf71741e7ffea
Rémy HUBSCHER <rhubscher@mozilla.com>
2017-10-02 16:24:56 +0200

    AMO leftover.

--- amo2kinto/amo.py
+++ amo2kinto/amo.py
@@ -5,20 +5,3 @@ import uuid
 def make_id_from_string(string):
     hashed = hashlib.md5(string.encode('utf-8'))
     return str(uuid.UUID(hashed.hexdigest()))
-
-
-def prepare_amo_records(records, fields):
-    for record in records:
-        blockID = record['blockID']
-
-        # Remove fields we do not want to keep.
-        for field in list(record.keys()):
-            if field not in fields:
-                del record[field]
-            else:
-                if isinstance(record[field], str):
-                    record[field] = record[field].strip()
-
-        record['id'] = make_id_from_string(blockID)
-
-    return records  # for convenience
